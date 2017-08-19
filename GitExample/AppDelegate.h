24824572d6a59879925d8c3d1098a27c4c9ad3a0
wangfei <wangfei1@tiens.com>
2017-08-19 13:38:47 +0800

    Initial Commit

---
+++ GitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitExample
+//
+//  Created by wangfei on 2017/8/19.
+//  Copyright © 2017年 wangfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitExample
+//
+//  Created by wangfei on 2017/8/19.
+//  Copyright © 2017年 wangfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
