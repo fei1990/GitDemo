9a1b9b9bb5aa2cbd1b10c994b1970336abb5a235
fei1990 <f.wang1990@hotmail.com>
2018-03-12 14:31:08 +0800

    merge

---
+++ GitExample/BModel.m
@@ -0,0 +1,20 @@
+//
+//  BModel.m
+//  GitExample
+//
+//  Created by wangfei on 2018/3/12.
+//  Copyright © 2018年 wangfei. All rights reserved.
+//
+
+#import "BModel.h"
+
+@implementation BModel
+
+- (instancetype)init {
+    if (self = [super init]) {
+        NSLog(@"%s", __func__);
+    }
+    return self;
+}
+
+@end
