582d78eb6fa9f7b3c3d5341d04f090f5d384f533
fei1990 <f.wang1990@hotmail.com>
2018-03-12 11:08:45 +0800

    创建文件Amodel

---
+++ GitExample/Amodel.m
@@ -0,0 +1,13 @@
+//
+//  Amodel.m
+//  GitExample
+//
+//  Created by wangfei on 2018/3/12.
+//  Copyright © 2018年 wangfei. All rights reserved.
+//
+
+#import "Amodel.h"
+
+@implementation Amodel
+
+@end
